8b6e2ee8c98a3d52d80026692a516a021eada740
Giorgio Ventura <gventuraagramonte@gmail.com>
2020-01-19 20:48:41 -0500

    Finalizando CRUD React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoForm from "./components/TodoForm";
+import { todos } from "./todos.json";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this)
+    
+  }
+
+  handleAddTodo(todo){
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index){
+    if(window.confirm('Are you sure you want to delete it?')){
+      this.setState({
+        todos: this.state.todos.filter((e, i)=>{
+          return i!== index
+        })
+      })
+    }
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p>
+                <mark>{todo.responsible}</mark>
+              </p>
+            </div>
+            <div className="card-footer">
+              <button className="btn btn-danger" onClick={this.removeTodo.bind(this, i)}>Delete</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-White bold">
+            Tasks
+            <span className="Badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-4 text-center">
+              <img
+                src={logo}
+                className="App-logo"
+                alt="logo"
+                style={{ height: 70, width: 70 }}
+              />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+            <div className="col-md-8">
+              <div className="row">{todos}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
